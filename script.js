94165f9fa16836a22dafcb218484aa1f6c234e4f
Muhamad fikri <158835175+tapirCode@users.noreply.github.com>
2026-03-01 04:19:04 +0800

    Update script.js with new search functionality

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-const searchInput= document.getElementById('search-input')
-const searchResult= document.getElementById('search-result')
+const searchInput = document.getElementById('search-input')
+const searchResult = document.getElementById('search-result')
 
 fetch('data.json')
- .then(response => response.json())
- .then(data =>{
-   const categories = data.categories
-   searchInput.addEventListener("input",(e)=> {
-    const searchQuery =e.target.value.trim().toLowerCase()
-    searchResult.innerHTML=""
-    
-     categories.forEach(category =>{
-     category.menus.forEach(menu=>{
-if (menu.name.toLowerCase().includes(searchQuery)) {
-     const li= document.createElement('li')
-     li.innerHTML = `
-  <span class="menu-name">${menu.name}</span><br>
-  <span class="menu-ingredients">${menu.ingredients.map(ingredient=> `<li>${ingredient}</li>`).join('')}</span><br>
-`;
-     searchResult.appendChild(li)
-     }
-   })
-})
-     })
- })
+  .then(response => response.json())
+  .then(data => {
+    const categories = data.categories
+    searchInput.addEventListener("input", (e) => {
+      const searchQuery = e.target.value.trim().toLowerCase()
+      searchResult.innerHTML = ""
+      
+      categories.forEach(category => {
+        category.menus.forEach(menu => {
+          if (menu.name.toLowerCase().includes(searchQuery)) {
+            const li = document.createElement('li')
+            li.innerHTML = `
+              <span class="menu-name">${menu.name}</span>
+              <ul class="menu-ingredients">
+                ${menu.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+              </ul>
+            `
+            searchResult.appendChild(li)
+          }
+        })
+      })
+    })
+  })
